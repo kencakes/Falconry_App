0a229c30c622f7bdf737d50584696534fb9a0e41
Kendra Vansaet <60871353+kencakes@users.noreply.github.com>
2021-12-29 17:24:50 +0100

    Changed config file

---
+++ capacitor.config.ts
@@ -0,0 +1,10 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.vansaet.kendra.falconry',
+  appName: 'falconry',
+  webDir: 'www',
+  bundledWebRuntime: false
+};
+
+export default config;
