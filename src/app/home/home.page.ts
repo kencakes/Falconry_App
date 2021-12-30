14098ecce31af1068a8d7ee8ff9b7f8e82fc8dcd
Kendra Vansaet <60871353+kencakes@users.noreply.github.com>
2021-12-30 09:59:46 +0100

    revert

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { AlertController, ModalController } from '@ionic/angular';
 import { BirdService, Bird } from '../services/bird.service';
 import { BirdDetailsPage } from '../bird-details/bird-details.page';
 
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -13,8 +14,8 @@ export class HomePage{
   searchTerm: string;
   birds: Bird[] = [];
 
-  constructor(private birdService: BirdService, private cd: ChangeDetectorRef, private alertCtrl: AlertController,
-              private modalController: ModalController) {
+  constructor(private birdService: BirdService, private cd: ChangeDetectorRef,
+              private alertCtrl: AlertController, private modalController: ModalController) {
     this.birdService.getBirds().subscribe(res => {
       this.birds = res;
       this.cd.detectChanges();
