bb676d4e8d50c7e424a6c4593a844ce12845325a
Kendra Vansaet <60871353+kencakes@users.noreply.github.com>
2021-12-30 10:51:26 +0100

    changed appname

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,7 +1,7 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'io.ionic.starter',
+  appId: 'com.vansaet.kendra.falconry',
   appName: 'falconry',
   webDir: 'www',
   bundledWebRuntime: false
