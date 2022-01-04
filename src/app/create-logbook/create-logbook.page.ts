201de939a3b9d4b6ef7b52a0cd21f780eaa9962d
Kendra Vansaet <60871353+kencakes@users.noreply.github.com>
2022-01-04 14:32:53 +0100

    Toevoegen button routing

--- src/app/create-logbook/create-logbook.page.ts
+++ src/app/create-logbook/create-logbook.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../services/auth.service';
 import { Logbook } from '../types/logbook';
+import { LogbookService } from '../services/logbook.service';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-create-logbook',
@@ -18,9 +20,13 @@ export class CreateLogbookPage implements OnInit {
   newTime = '';
   newWeight = 0;
 
-  constructor(public authService: AuthService) { }
+  constructor(public authService: AuthService, private logbookService: LogbookService, private router: Router) { }
 
   ngOnInit() {
+  }
+
+  async createLogbook(): Promise<void>{
+    await this.logbookService.creatLogbook(this.newName, this.newFood, this.newAmount, this.newDate, this.newTime, this.newWeight);
   }
 
 }
